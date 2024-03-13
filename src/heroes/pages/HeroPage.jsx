ebecd9aba1c14580939142ab9eba58bb71339959
Spiderpaul <paulugortega@gmai.com>
2024-03-13 11:58:47 -0700

    :)

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -22,7 +22,7 @@ export const HeroPage = () => {
         <div className="row mt-5">
             <div className="col-4">
                 <img 
-                    src={ `/heroes/${ hero.id }.jpg` } 
+                    src={ `/img/${ hero.id }.jpg` } 
                     alt={ hero.superhero } 
                     className="img-thumbnail animate__animated animate__bounceInLeft"
                 />
